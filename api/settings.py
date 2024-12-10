12e948d7cf59a39ed298e831ac6ca6bf322ebaea
karthikeyan-ks <karthikeyanks3673@gmail.com>
2024-12-11 00:48:19 +0530

    database

--- api/settings.py
+++ api/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'api.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teklearn',
+        'USER': 'teklearn',
+        'PASSWORD': 'teklearn@123!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
