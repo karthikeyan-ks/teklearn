9e5d14b0a0e1066401dd36db3947af5e139744fc
karthikeyan-ks <karthikeyanks3673@gmail.com>
2024-12-11 00:40:17 +0530

    csrf

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','teklearn.onrender.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,12 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:5173',  # Add the URL of your React app
+    'https://teklearn.onrender.com',  # Add the URL of your React app
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'https://teklearn.onrender.com',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
